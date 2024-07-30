67ddd1ca366a79300fce623dfaed0b71dfcf6dea
Torstein <141224356+Trind03@users.noreply.github.com>
2024-07-30 11:01:07 +0200

    Added code without top level statements

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace program
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            System.Console.Write("Hello World");
+        }
+    }
+}
