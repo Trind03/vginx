6b5efc9f665ef127be1e0bf5e136d192acdc1b04
Torstein <141224356+Trind03@users.noreply.github.com>
2024-07-31 10:54:26 +0200

    Update
    Added source code to handle index.html not found. And responding with error h1 tags.

--- Program.cs
+++ Program.cs
@@ -6,13 +6,24 @@ namespace program
 {
     internal class Program
     {
+        public static byte[] Response = {};
         public static void Main()
         {
             HttpListener Server = new HttpListener();
             Server.Prefixes.Add("0.0.0.0::8080");
 
             // ** Open for refactoring - Possible null referance from this expression **
-            byte[] Response = System.Text.Encoding.UTF8.GetBytes(File.ReadAllLines("./index.html").ToString());
+            if(File.Exists("./index.html"))
+            {
+                string? Data = File.ReadAllLines("./index.html").ToString();
+                Response = System.Text.Encoding.UTF8.GetBytes(Data);
+            }
+            else
+            {
+                string? Data = "<h1>Error data source not found :< </h1>";
+                Response = System.Text.Encoding.UTF8.GetBytes(Data);
+            }
+
         }
     }
 }
