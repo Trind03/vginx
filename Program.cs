8a145057b7b4e60c6620923e9910c9af36b6b57f
Torstein <141224356+Trind03@users.noreply.github.com>
2024-07-31 20:28:20 +0200

    Update
    Moved server logic

--- Program.cs
+++ Program.cs
@@ -11,29 +11,7 @@ namespace program
         {
             Server server = new Server(8080);
 
-            if(File.Exists("./index.html"))
-            {
-                string? Data = "";
-                foreach(string c in File.ReadAllLines("./index.html"))  Data = Data + c;
-                if(Data == null)
-                {
-                    server.Buffer = System.Text.Encoding.UTF8.GetBytes("<h1>Server error, null referance :< </h1>");
-                    return -1;
-                }
-                server.Buffer = System.Text.Encoding.UTF8.GetBytes(Data);
-            }
-            else
-            {
-                string? Data = "<h1>Error data source not found :< </h1>";
-                server.Buffer = System.Text.Encoding.UTF8.GetBytes(Data);
-            }
-            server.Context = server.Listener.GetContext();
-            server.Request = server.Context.Request;
-            server.Response = server.Context.Response;
-
-            server.Response.ContentLength64 = server.Buffer.Length;
-            System.IO.Stream Output = server.Response.OutputStream;
-            Output.Write(server.Buffer,0,server.Buffer.Length);
+            server.start_server();
             return 0;
         }
     }
