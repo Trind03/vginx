34be4bad2f6dd6df319d3b0ff4188d60145d6e39
Torstein <141224356+Trind03@users.noreply.github.com>
2024-08-03 16:39:40 +0200

    Added class

---
+++ command.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace server
+{
+    internal class Command
+    {
+        Command()
+        {
+            _running_status = true;
+        }
+
+
+        private bool _running_status;
+    }
+}
