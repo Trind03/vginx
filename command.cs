79958128154055a1eaddbcdcc39b8aa94f29f1f2
Torstein <141224356+Trind03@users.noreply.github.com>
2024-08-03 17:36:40 +0200

    Added offset for cursor pos, added main loop.

--- command.cs
+++ command.cs
@@ -1,16 +1,30 @@
 using System;
-
+using System.Drawing;
 
 namespace server
 {
     internal class Command
     {
-        Command()
+        Command(int x, int y)
         {
+            _offset_x = x;
+            _offset_y = y;
             _running_status = true;
         }
 
+        public void command_handler()
+        {
+            while(_running_status)
+            {
+
+                System.Console.Write("> ");
+                System.Console.ReadLine();
+            }
+        }
 
         private bool _running_status;
+        private string _command;
+        private int _offset_x;
+        private int _offset_y;
     }
 }
