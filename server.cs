549df9d3fa43519e6661dbdb52ba0864079c3de6
Torstein <141224356+Trind03@users.noreply.github.com>
2024-08-03 15:09:08 +0200

    Bugfix for response logic

--- server.cs
+++ server.cs
@@ -7,7 +7,7 @@ namespace server
 {
     internal class Server
     {
-        public string fetch_type(string filename)
+        public string fetchType(string filename)
         {
             switch(Path.GetExtension(filename))
             {
@@ -32,29 +32,31 @@ namespace server
         {
             while(running_status)
             {
-                if(File.Exists("./index.html"))
-                {
-                    string? Data = "";
-                    foreach(string c in File.ReadAllLines("./index.html"))  Data = Data + c;
-                    if(Data == null)
-                    {
-                        Buffer = System.Text.Encoding.UTF8.GetBytes("<h1>Server error, null referance :< </h1>");
-                        return 1;
-                    }
-                    Buffer = System.Text.Encoding.UTF8.GetBytes(Data);
-                }
-                else
-                {
-                    string? Data = "<h1>Error data source not found :< </h1>";
-                    Buffer = System.Text.Encoding.UTF8.GetBytes(Data);
-                }
-
                 Context = Listener.GetContext();
                 Request = Context.Request;
-                string Requested_file = Request.Url.LocalPath.Trim('/');
                 Response = Context.Response;
-                System.Console.Write(Requested_file,"Hello world");
                 Response.ContentLength64 = Buffer.Length;
+                
+                string RequestedFile = Request.Url.LocalPath.Trim('/');
+                if(string.IsNullOrEmpty(RequestedFile))
+                {
+                    RequestedFile = "index.html";
+                }
+
+                if(File.Exists(RequestedFile))
+                {
+                    Buffer = File.ReadAllBytes(RequestedFile);
+                    Response.ContentLength64 = Buffer.Length;
+                    Response.ContentType = fetchType(RequestedFile);
+                    Response.OutputStream.Write(Buffer,0,Buffer.Length);
+                }
+
+                else
+                {
+                    Buffer = System.Text.Encoding.UTF8.GetBytes("<h1>Error data source not found :< </h1>");
+                }
+
+                
                 System.IO.Stream Output = Response.OutputStream;
                 Output.Write(Buffer,0,Buffer.Length);
             }
