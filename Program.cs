dc3f21aa563a3f1009f145dbbcafa293f8f374cd
Torstein <141224356+Trind03@users.noreply.github.com>
2024-09-16 13:21:24 +0200

    Update
    Allowed for multi threading for command listening.

--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@ using System.IO;
 using System.Threading;
 using app.server;
 
-
 namespace program
 {
     internal class Program
     {
         public static int Main()
         {
+            Command _Command = new Command(0,0);
+            Thread _Command_h = new Thread(_Command.command_handler);
             Server server = new Server(8080);
             server.start_server();
             return 0;
